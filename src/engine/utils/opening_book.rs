e80dae1de61edfcd3b4c01a5140d510a70cb934a
Trevor Swan <tcs94@case.edu>
2025-08-13 09:27:47 -0400

    static option for opening book

--- src/engine/utils/opening_book.rs
+++ src/engine/utils/opening_book.rs
@@ -1,6 +1,19 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::OnceLock};
 
 use rand::{SeedableRng, rngs::StdRng};
+
+use crate::engine::game::board::Board;
+
+static OPENING_BOOK: OnceLock<OpeningBook> = OnceLock::new();
+
+fn init_book() -> OpeningBook {
+    OpeningBook::new(BOOK)
+}
+
+/// Retrieve the book instance containing "assets/book.txt"
+pub fn get_book() -> &'static OpeningBook {
+    OPENING_BOOK.get_or_init(init_book)
+}
 
 const BOOK: &str = include_str!("../../../assets/book.txt");
 
@@ -19,13 +32,34 @@ impl Default for OpeningBook {
 }
 
 impl OpeningBook {
-    pub fn new(file: &str) -> Self {
+    pub fn new(file_contents: &str) -> Self {
         let mut book = OpeningBook::default();
+        let entries: Vec<&str> = file_contents.trim_matches(&[',', '\n']).split("pos").collect();
+        book.moves_by_position.reserve(entries.len());
+        
+        for entry in entries {
+            let entry_data: Vec<&str> = entry.trim_matches('\n').split('\n').collect();
+            let position_fen = entry_data[0].trim();
+            let all_move_data = entry_data[1..].to_vec();
+
+            let mut book_moves = Vec::with_capacity(all_move_data.len());
+
+            for move_idx in 0..all_move_data.len() {
+                let move_data: Vec<&str> = all_move_data[move_idx].split_whitespace().collect();
+                book_moves[move_idx] = BookMove::new(move_data[0].to_string(), move_data[1].parse().unwrap_or(0));
+            }
+
+            book.moves_by_position.insert(position_fen.to_string(), book_moves);
+        }
         book
     }
 
     pub fn has_book_move(&self, position_fen: String) -> bool {
         self.moves_by_position.contains_key(&Self::remove_move_counters_from_fen(position_fen))
+    }
+
+    pub fn try_get_book_move(board: &Board, weight_pow: f32) -> Option<String> {
+        todo!()
     }
 
     fn remove_move_counters_from_fen(fen: String) -> String {
